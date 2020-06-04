40300a358a4f34a854e41825f924a02ea0eed039
mhdhassan99 <mhassan.work99@gmail.com>
2020-06-04 16:49:14 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -19,10 +19,10 @@ class Owner
   def self.all
     @@all 
   end
-  def self.owners
+  def self.count
     @@all.count 
   end
-  def self.reset
+  def self.reset_all
     @@all.clear
   end
 
@@ -37,4 +37,31 @@ class Owner
       single_dog.owner == self 
     end
   end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end 
+  def feed_cats 
+    self.cats.each {|cat| cat.mood = "happy"}
+  end 
+
+  def sell_pets 
+    pets = self.cats + self.dogs
+    pets.each do |pet| 
+      pet.mood = "nervous"
+      pet.owner = nil 
+    end
+  end 
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
+
 end
