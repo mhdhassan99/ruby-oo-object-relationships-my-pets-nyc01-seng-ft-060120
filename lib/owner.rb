40d914070ca9feb909a3c2d7054e4a15f8a82e08
mhdhassan99 <mhassan.work99@gmail.com>
2020-06-04 15:01:01 -0400

    pair_working_file

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
 class Owner
-  # code goes here
+
+  attr_reader :name 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @species = species
+    @@all << self 
+  end
+
+  def species 
+    @species = "human"
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+ 
+  def self.all
+    @@all 
+  end
+  def self.owners
+    @@all.count 
+  end
+  def self.reset
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select do |single_cat|
+      single_cat.owner == self 
+    end  
+  end
+
+  def dogs 
+    Dog.all.select do |single_dog|
+      single_dog.owner == self 
+    end
+  end
 end
